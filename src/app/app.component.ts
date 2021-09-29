2ae5c1fb5851b53d573ec2fe3fbfe9d2dab7e98f
John Holmes <john.holmes@weknowtraining.ca>
2021-09-29 15:19:22 -0600

    more places

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  restaurants: string[] = ["McDonalds", "Campio", "Remedy"];
+  restaurants: string[] = ["McDonalds", "Campio", "Remedy", "Tres Carnales", "Kellys", "Lets Grill", "Dorinku"];
   theChosenOne: string = "";
   showFood: boolean = false;
 
