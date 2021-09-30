34c8585e375c2a52c1c774ecb41e3b42261d69d7
John Holmes <john.holmes@weknowtraining.ca>
2021-09-30 10:11:58 -0600

    basic functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  restaurants: string[] = ["McDonalds", "Campio", "Remedy", "Tres Carnales", "Kellys", "Lets Grill", "Dorinku"];
+  restaurants: string[] = ["McDonalds", "Campio", "Remedy", "Tres Carnales", "Kellys", "Lets Grill", "Dorinku", "Chicken For Lunch", "Farrow", "CRAFT Beer Market", "Underground Tap & Grill", "Splash Poke", "The Burgers Priest", "Kyoto Sushi", "Northern Chicken"];
   theChosenOne: string = "";
   showFood: boolean = false;
 
   randomRestaurant(restaurants: string[]) {
     this.showFood = true;
     this.theChosenOne = restaurants[Math.floor(Math.random() * restaurants.length)];
+  }
+
+  correctAnswer() {
+    this.showFood = true;
+    this.theChosenOne = "McDonalds";
   }
 }
